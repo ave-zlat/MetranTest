db61e24d9bfb9501cc460daefd05f4a1f5646260
Alyona <ave-zlat@mail.ru>
2025-03-06 23:27:05 +0500

    first working version

---
+++ testApp/TestApp.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace testApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartTest = new System.Windows.Forms.Button();
+            this.btnStopTest = new System.Windows.Forms.Button();
+            this.lblTxtIdentifier = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.cmbTestSelection = new System.Windows.Forms.ComboBox();
+            this.txtIdentifier = new System.Windows.Forms.TextBox();
+            this.lblChooseTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartTest
+            // 
+            this.btnStartTest.Location = new System.Drawing.Point(27, 213);
+            this.btnStartTest.Name = "btnStartTest";
+            this.btnStartTest.Size = new System.Drawing.Size(139, 48);
+            this.btnStartTest.TabIndex = 0;
+            this.btnStartTest.Text = "Запуск теста";
+            this.btnStartTest.UseVisualStyleBackColor = true;
+            this.btnStartTest.Click += new System.EventHandler(this.btnStartTest_Click);
+            // 
+            // btnStopTest
+            // 
+            this.btnStopTest.Location = new System.Drawing.Point(227, 213);
+            this.btnStopTest.Name = "btnStopTest";
+            this.btnStopTest.Size = new System.Drawing.Size(155, 58);
+            this.btnStopTest.TabIndex = 1;
+            this.btnStopTest.Text = "Остановка теста";
+            this.btnStopTest.UseVisualStyleBackColor = true;
+            this.btnStopTest.Click += new System.EventHandler(this.btnStopTest_Click);
+            // 
+            // lblTxtIdentifier
+            // 
+            this.lblTxtIdentifier.AutoSize = true;
+            this.lblTxtIdentifier.Location = new System.Drawing.Point(12, 32);
+            this.lblTxtIdentifier.Name = "lblTxtIdentifier";
+            this.lblTxtIdentifier.Size = new System.Drawing.Size(228, 16);
+            this.lblTxtIdentifier.TabIndex = 2;
+            this.lblTxtIdentifier.Text = "Введите идентификатор изделия";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(87, 305);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(53, 16);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Статус";
+            // 
+            // cmbTestSelection
+            // 
+            this.cmbTestSelection.FormattingEnabled = true;
+            this.cmbTestSelection.Location = new System.Drawing.Point(15, 150);
+            this.cmbTestSelection.Name = "cmbTestSelection";
+            this.cmbTestSelection.Size = new System.Drawing.Size(241, 24);
+            this.cmbTestSelection.TabIndex = 4;
+            // 
+            // txtIdentifier
+            // 
+            this.txtIdentifier.Location = new System.Drawing.Point(27, 62);
+            this.txtIdentifier.Name = "txtIdentifier";
+            this.txtIdentifier.Size = new System.Drawing.Size(173, 22);
+            this.txtIdentifier.TabIndex = 5;
+            // 
+            // lblChooseTest
+            // 
+            this.lblChooseTest.AutoSize = true;
+            this.lblChooseTest.Location = new System.Drawing.Point(52, 110);
+            this.lblChooseTest.Name = "lblChooseTest";
+            this.lblChooseTest.Size = new System.Drawing.Size(104, 16);
+            this.lblChooseTest.TabIndex = 6;
+            this.lblChooseTest.Text = "Выберите тест";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblChooseTest);
+            this.Controls.Add(this.txtIdentifier);
+            this.Controls.Add(this.cmbTestSelection);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblTxtIdentifier);
+            this.Controls.Add(this.btnStopTest);
+            this.Controls.Add(this.btnStartTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartTest;
+        private System.Windows.Forms.Button btnStopTest;
+        private System.Windows.Forms.Label lblTxtIdentifier;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ComboBox cmbTestSelection;
+        private System.Windows.Forms.TextBox txtIdentifier;
+        private System.Windows.Forms.Label lblChooseTest;
+    }
+}
+
---
+++ testApp/TestApp.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testApp
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource _cts;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnStartTest_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtIdentifier.Text))
+            {
+                MessageBox.Show("Введите идентификатор изделия!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (cmbTestSelection.SelectedIndex == -1)
+            {
+                MessageBox.Show("Выберите тест!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _cts = new CancellationTokenSource();
+            btnStartTest.Enabled = false;
+            btnStopTest.Enabled = true;
+            lblStatus.Text = "Идет тестирование...";
+
+            try
+            {
+                var result = await RunTestAsync(cmbTestSelection.SelectedIndex, _cts.Token);
+                SaveResults(txtIdentifier.Text, result);
+                lblStatus.Text = "Тест завершен.";
+                MessageBox.Show("Тест завершен. Результаты сохранены.", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (OperationCanceledException)
+            {
+                lblStatus.Text = "Тест отменен.";
+                MessageBox.Show("Тест был остановлен пользователем.", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            finally
+            {
+
+                btnStopTest.Enabled = false;
+            }
+        }
+
+
+        private void btnStopTest_Click(object sender, EventArgs e)
+        {
+            _cts?.Cancel();
+        }
+
+        private async Task<TestResult> RunTestAsync(int testIndex, CancellationToken token)
+        {
+            Random rand = new Random();
+            int delay = rand.Next(10, 31) * 1000;
+            await Task.Delay(delay, token);
+
+            var result = new TestResult
+            {
+                Success = rand.Next(2) == 0,
+                ErrorMessage = null,
+                Data = new StringBuilder()
+            };
+
+            if (!result.Success)
+            {
+                result.ErrorMessage = "Ошибка #" + rand.Next(100, 999);
+            }
+
+            switch (testIndex)
+            {
+                case 0:
+                    result.Data.AppendLine($"Температура: {rand.Next(-50, 100)}°C");
+                    break;
+                case 1:
+                    result.Data.AppendLine($"Напряжение: {rand.NextDouble() * 220:F2}V");
+                    result.Data.AppendLine($"Ток: {rand.NextDouble() * 10:F2}A");
+                    break;
+                case 2:
+                    result.Data.AppendLine($"Частота: {rand.Next(1000, 5000)} Гц");
+                    result.Data.AppendLine($"Коэффициент мощности: {rand.NextDouble():F2}");
+                    result.Data.AppendLine($"Сопротивление: {rand.Next(1, 1000)} Ом");
+                    break;
+            }
+
+            return result;
+        }
+
+        private void SaveResults(string productId, TestResult result)
+        {
+            string directoryPath = "D:\\Worrk\\results";
+            Directory.CreateDirectory(directoryPath);
+
+            string filePath = Path.Combine(directoryPath, $"{productId}.txt");
+            using (StreamWriter writer = new StreamWriter(filePath, false, Encoding.UTF8))
+            {
+                writer.WriteLine($"Результат теста: {(result.Success ? "Успешно" : "Ошибка")}");
+                if (!string.IsNullOrEmpty(result.ErrorMessage))
+                    writer.WriteLine($"Ошибка: {result.ErrorMessage}");
+
+                writer.WriteLine("Данные:");
+                writer.WriteLine(result.Data.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Ожидается ввод...";
+
+            cmbTestSelection.Items.Add("Тест 1 (Температура)");
+            cmbTestSelection.Items.Add("Тест 2 (Напряжение и ток)");
+            cmbTestSelection.Items.Add("Тест 3 (Частота, КПД, сопротивление)");
+            cmbTestSelection.SelectedIndex = 0;
+
+            btnStopTest.Enabled = false;
+        }
+
+        public class TestResult
+        {
+            public bool Success { get; set; }
+            public string ErrorMessage { get; set; }
+            public StringBuilder Data { get; set; }
+        }
+    }
+}
+
